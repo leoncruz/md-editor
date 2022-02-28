b0c088b2fccaa143e195ecf069ffc804bc6076c7
Leon Cruz <leon.cruz.teixeira@gmail.com>
2022-02-28 15:50:44 -0300

    initial configuration

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'standard'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier', 'react-hooks'],
+  rules: {
+    'no-console': 'error',
+    'no-unused-expressions': 'off',
+    'prettier/prettier': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    semi: [2, 'always'],
+    quotes: ['error', 'single'],
+    'comma-dangle': ['error', 'never'],
+    'no-use-before-define': 'off'
+  }
+};
